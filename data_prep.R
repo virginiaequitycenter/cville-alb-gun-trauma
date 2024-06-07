d66b1b203cfdc3d7d1ac1c78785958c82a732ab4
Samantha Toet <samanthactoet@gmail.com>
2024-06-07 12:22:17 -0400

    clean up empty space

--- data_prep.R
+++ data_prep.R
@@ -130,7 +130,10 @@ write_csv(pops, "data/ucr_pops.csv")
 
 # *Theft ----
 
-theft <- read_csv("data/raw/nibrs_theft.csv")
+# This data is surprisingly already tidy and properly formatted so we don't have to do anything :) 
+
+nibrs_theft <- read_csv("data/raw/nibrs_theft.csv")
+write_csv(nibrs_theft, "data/nibrs_theft.csv")
 
 # ATF ----
 

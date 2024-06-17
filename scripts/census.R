10f8f3f54a1a4574aa0dc54a58b2f6a80850a898
Samantha Toet <samanthactoet@gmail.com>
2024-06-17 13:43:47 -0400

    remove conditional coloring from leaflet clustering

--- scripts/census.R
+++ scripts/census.R
@@ -2,12 +2,49 @@
 
 # Setup ----
 library(ggmap)
+library(janitor)
 library(leaflet)
+library(lubridate)
 library(sf)
+library(readxl)
+library(stringr)
 library(tidycensus)
 library(tidyverse)
 
-# Census prep
+# *Gun violence data prep ----
+# All incidents of gun violence from 2019 - 2024
+all_gv <- read_excel("data/raw/Regional GV Data - 2019-2024 YTD_BLOCK ADDRESS.xlsx") %>%
+  clean_names()
+
+all_gv <- all_gv %>%
+  mutate(locality = case_when(
+    agency == "ACPD" ~ "Albemarle County",
+    TRUE ~ "Charlottesville"))
+
+# Filter to only include verified shots fired (Shots Fired, 1, 2, 3), murder (09A), Active Shooter, and aggravated assaults (13A)
+gv <- all_gv %>%
+  filter(str_detect(crime_code, "Shots|09A|13A|Active Shooter"),
+         !str_detect(verified, "^UN"))
+
+gv <- gv %>% 
+  mutate(description = case_when(
+    crime_code == "09A" ~ "Homicide",
+    crime_code == "13A" ~ "Aggravated Assault",
+    crime_code == "Active Shooter" ~ "Active Shooter",
+    TRUE ~ "Shots Fired"
+  ))
+
+
+# Geocode
+gv <- gv %>%
+  mutate(address = paste(block_address, locality))
+#lon_lat <- geocode(gv$address)
+sum(is.na(lon_lat$lon)) #0 
+gv <- bind_cols(gv, lon_lat)
+
+#write_csv(gv, "data/gun_violence.csv")
+
+# *Census data prep ----
 county_codes <- data.frame(code = c(540, 003), 
                            name = c("Albemarle County", "Charlottesville City"))
 region <- county_codes$code
@@ -17,7 +54,17 @@ tract_names <- read_csv("data/tract_names.csv")
 # Initial ACS variables to explore:
 vars <- c("B01003_001",    # population
           "S1701_C03_001", # poverty rate
-          "S1701_C03_002") # child poverty rate 
+          "S1701_C03_002", # child poverty rate 
+          "S2301_C04_001", # Percent unemployment (Population 16 and over)
+          "B20002_001E",   # median earnings 12m age 16+
+          "B20004_001E",   # median earnings 12m age 25+
+          "B20004_002E",   # median earnings 12m age 25+ < high school
+          "B20004_003E",   # median earnings 12m age 25+ high school grad
+          "B20004_004E",   # median earnings 12m age 25+ some college or associates
+          "B20004_005E",   # median earnings 12m age 25+ bachelor's 
+          "B20004_006E")   # median earnings 12m age 25+ graduate or professional degree 
+
+# TODO:  HDI 
 
 # 2018-2022 5-year ACS
 dat <- get_acs(geography = "tract",
@@ -33,107 +80,26 @@ dat <- dat %>%
   select(-ends_with("M")) %>%
   rename(pop_est = B01003_001E,
          poverty_est = S1701_C03_001E,
-         cpov_est = S1701_C03_002E) %>%
+         cpov_est = S1701_C03_002E,
+         unemployment_rate = S2301_C04_001E,
+         med_earn_16 = B20002_001E,
+         med_earn_25 = B20004_001E,
+         med_earn_nohs = B20004_002E,
+         med_earn_hs = B20004_003E,
+         med_earn_col = B20004_004E,
+         med_earn_bd = B20004_005E,
+         med_earn_gd = B20004_006E) %>%
   separate_wider_delim(NAME, delim = "; ", names = c("tract", "locality", "state")) %>%
   left_join(tract_names, by = join_by(tract == tract_id)) %>%
   st_as_sf()
   
-#write_csv(dat, "data/acs_pov.csv")
+#write_csv(dat, "data/tmp_census.csv")
 
-# Albemarle  ----
+# Poverty & Childhood poverty ----
+pal_pov <- colorNumeric(palette = "viridis",
+                        domain = NULL,
+                        reverse = TRUE)
 
-alb_map <- get_map(c(left = -79, bottom = 37, right = -78, top = 39),
-                   maptype = "roadmap", color = "bw")
-  
-# Poverty 
-ggmap(alb_map) +
-  geom_sf(data = dat, 
-          aes(fill = poverty_est, 
-              geometry = geometry), 
-          inherit.aes = FALSE, alpha = .8) +
-  scale_fill_viridis_c(direction = -1)
-  
-
-# Child poverty 
-ggmap(alb_map) +
-  geom_sf(data = dat, 
-          aes(fill = cpov_est, 
-              geometry = geometry), 
-          inherit.aes = FALSE, alpha = .8) +
-  scale_fill_viridis_c(direction = -1)
-
-# Cville  ----
-cville_map <- get_map(c(left = -78.54, bottom = 37.97, right = -78.43, top = 38.1),
-                      maptype = "roadmap", color = "bw")
-
-cville <- dat %>%
-  filter(locality == "Charlottesville city")
-
-# Poverty 
-ggmap(cville_map) +
-  geom_sf(data = cville, 
-          aes(fill = poverty_est, 
-              geometry = geometry), 
-          inherit.aes = FALSE, alpha = .8) +
-  scale_fill_viridis_c(direction = -1)
-
-
-# Child poverty 
-ggmap(cville_map) +
-  geom_sf(data = cville, 
-          aes(fill = cpov_est, 
-              geometry = geometry), 
-          inherit.aes = FALSE, alpha = .8) +
-  scale_fill_viridis_c(direction = -1)
-
-
-# Faceting ----
-alb_long <- dat %>%
-  pivot_longer(cols = c(poverty_est, cpov_est))
-
-facet_names <- c(
-  cpov_est = "Childhood Poverty",
-  poverty_est = "Overall Poverty")
-
-ggmap(alb_map) +
-  geom_sf(data = alb_long, 
-          aes(fill = value, 
-              geometry = geometry), 
-          inherit.aes = FALSE, alpha = .8) +
-  facet_wrap( ~ name, labeller = as_labeller(facet_names)) +
-  scale_fill_viridis_c(direction = -1, 
-                       name = "Rate", 
-                       labels=function(x) paste0(x,"%")) +
-  labs(title = "Poverty Rates in Albemarle County") +
-  theme(axis.title = element_blank(),
-        axis.text = element_blank(),
-        axis.ticks = element_blank())
-
-  
-cville_long <- cville %>%
-  pivot_longer(cols = c(poverty_est, cpov_est))
-
-ggmap(cville_map) +
-  geom_sf(data = cville_long, 
-          aes(fill = value, 
-              geometry = geometry), 
-          inherit.aes = FALSE, alpha = .8) +
-  facet_wrap( ~ name, labeller = as_labeller(facet_names)) +
-  scale_fill_viridis_c(direction = -1, 
-                       name = "Rate", 
-                       labels=function(x) paste0(x,"%")) +
-  labs(title = "Poverty Rates in Charlottesville City") +
-  theme(axis.title = element_blank(),
-        axis.text = element_blank(),
-        axis.ticks = element_blank())
-
-# Leaflet ----
-
-pal <- colorNumeric(palette = "viridis",
-                    domain = NULL,
-                    reverse = TRUE)
-
-# Poverty
 dat %>%
   st_transform(crs = 4326) %>%
   leaflet() %>%
@@ -143,7 +109,7 @@ dat %>%
               weight = 0.5,
               opacity = 1,
               color = "black", 
-              fillColor = ~ pal(poverty_est),
+              fillColor = ~ pal_pov(poverty_est),
               fillOpacity = 0.5,
               popup = paste0("Poverty Rate: ", dat$poverty_est, "%", "<br>",
                              "Population: ", dat$pop_est, "<br>",
@@ -157,7 +123,7 @@ dat %>%
               weight = 0.5,
               opacity = 1,
               color = "black", 
-              fillColor = ~ pal(cpov_est),
+              fillColor = ~ pal_pov(cpov_est),
               fillOpacity = 0.5,
               popup = paste0("Child Poverty Rate: ", dat$cpov_est, "%", "<br>",
                              "Population: ", dat$pop_est, "<br>",
@@ -167,17 +133,86 @@ dat %>%
                 bringToFront = FALSE
               )) %>%
   addLegend("bottomright",
-            pal = pal,
+            pal = pal_pov,
             values = ~ poverty_est, 
             title = "Estimated Poverty Rates",
             labFormat = labelFormat(suffix = "%"), 
             opacity = 1) %>%
   addLayersControl(baseGroups = c("Overall Poverty", "Child Poverty"),
-                   options = layersControlOptions(collapsed = FALSE))
+                   options = layersControlOptions(collapsed = FALSE)) %>%
+  addMarkers(data = gv, 
+             lng = gv$lon,
+             lat = gv$lat,
+             popup = paste0("Description: ", gv$description, "<br>",
+                            "Date: ", gv$reported_date),
+             clusterOptions = markerClusterOptions(
+               showCoverageOnHover = FALSE,
+               iconCreateFunction=JS("function (cluster) {    
+    var childCount = cluster.getChildCount();  
+    if (childCount < 100) {  
+      c = 'rgba(211,211,211);'
+    } else if (childCount < 1000) {  
+      c = 'rgba(211,211,211);'  
+    } else { 
+      c = 'rgb(211,211,211);'  
+    }    
+    return new L.DivIcon({ html: '<div style=\"background-color:'+c+'\"><span>' + childCount + '</span></div>', className: 'marker-cluster', iconSize: new L.Point(40, 40) });
+
+  }")
+             ))
 
 
- 
+# Median Income by Education ----
+pal_earn <- colorNumeric(palette = "viridis",
+                        domain = NULL,
+                        reverse = FALSE)
+
+dat %>%
+  st_transform(crs = 4326) %>%
+  leaflet() %>%
+  addProviderTiles(providers$CartoDB.Positron) %>% 
+  addPolygons(group = "All Education Levels",
+              stroke = TRUE, 
+              weight = 0.5,
+              opacity = 1,
+              color = "black", 
+              fillColor = ~ pal_earn(med_earn_25),
+              fillOpacity = 0.5,
+              popup = paste0("Median Earnings: ", scales::dollar(dat$med_earn_25), "<br>",
+                             "Poverty Rate: ", dat$poverty_est, "%", "<br>",
+                             "Population: ", dat$pop_est, "<br>",
+                             "Tract: ", dat$tract_name, ", ", dat$locality),
+              highlightOptions = highlightOptions(
+                fillOpacity = 1,
+                bringToFront = FALSE
+              )) %>%
+  addLegend("bottomright",
+            pal = pal_earn,
+            values = ~ med_earn_25, 
+            title = "Median Earnings",
+            labFormat = labelFormat(prefix = "$"), 
+            opacity = 1) %>%
+  addLayersControl(baseGroups = c("All Education Levels"),
+                   options = layersControlOptions(collapsed = FALSE)) %>%
+  addMarkers(data = gv, 
+             lng = gv$lon,
+             lat = gv$lat,
+             popup = paste0("Description: ", gv$description, "<br>",
+                            "Date: ", gv$reported_date),
+             clusterOptions = markerClusterOptions(
+               showCoverageOnHover = FALSE,
+               iconCreateFunction=JS("function (cluster) {    
+    var childCount = cluster.getChildCount();  
+    if (childCount < 100) {  
+      c = 'rgba(211,211,211);'
+    } else if (childCount < 1000) {  
+      c = 'rgba(211,211,211);'  
+    } else { 
+      c = 'rgb(211,211,211);'  
+    }    
+    return new L.DivIcon({ html: '<div style=\"background-color:'+c+'\"><span>' + childCount + '</span></div>', className: 'marker-cluster', iconSize: new L.Point(40, 40) });
+
+  }")
+             ))
 
   
-
-

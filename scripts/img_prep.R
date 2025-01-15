4133f7534bf6bdaa055210dc08be65dee273b1b2
Samantha Toet <samanthactoet@gmail.com>
2025-01-15 12:19:28 -0500

    modify images for print and fix notable trends formatting

--- scripts/img_prep.R
+++ scripts/img_prep.R
@@ -1,10 +1,15 @@
+library(ggrepel)
 library(janitor)
 library(lubridate)
+library(sf)
+library(svglite)
 library(tidyverse)
 
-# Format images for print media 
+# Format images for print media and slide decks
 
-# N Victims Over Time 2024 ----
+# N Victims Over Time by Month ----
+# Data: Gun Violence Archive 
+# Date range: Jan 2018 - Dec 2024
 
 gva <- read_csv("data/raw/gva_incidents_24.csv") %>%
   clean_names() %>%
@@ -13,18 +18,6 @@ gva <- read_csv("data/raw/gva_incidents_24.csv") %>%
 
 gva$month_year <- floor_date(gva$incident_date, unit = "month")
 
-gva %>%
-  filter(!grepl(2025, incident_date)) %>%
-  group_by(month_year) %>%
-  summarise(total_injured = sum(victims_injured + suspects_injured),
-            total_killed = sum(victims_killed + suspects_killed)) %>%
-  ungroup() %>%
-  pivot_longer(matches("total")) %>%
-  filter(!value == 0) %>% 
-  ggplot(aes(x = month_year, y = value, color = name)) +
-  geom_col()
-
-  
 gva %>%
   filter(between(incident_date, mdy("12-31-2017"), mdy("12-31-2024"))) %>%
   group_by(month_year) %>%
@@ -47,24 +40,113 @@ gva %>%
         axis.text.x = element_text(angle = 25))
 
 
-# Aug 22 - March 23
-gva_narrow <- gva %>%
-  filter(between(incident_date, mdy("8-1-2022"), mdy("3-31-2023")))
+# N Incidents Over Time by Month ----
+# Data: Local Police Reports 
+# Date Range: Jan 1 2019 - May 18 2024
+
+gv <- read_csv("data/regional_gv.csv") %>%
+  mutate(reported_date = ymd(as.Date(reported_date)))
+
+gv$month_year <- floor_date(gv$reported_date, unit = "month")
+
+gv %>%
+  group_by(month_year) %>% 
+  summarise(count=n()) %>%
+  ggplot(aes(x = month_year, y = count)) +
+  geom_line()
+
+# Combine
+
+gva_plt <- gva %>%
+  filter(between(incident_date, mdy("12-31-2017"), mdy("12-31-2024"))) %>%
+  group_by(month_year) %>%
+  summarise(total_injured = sum(victims_injured + suspects_injured),
+            total_killed = sum(victims_killed + suspects_killed)) %>%
+  ungroup() %>%
+  pivot_longer(matches("total"))
+
+gv_plt <- gv %>%
+  group_by(month_year) %>% 
+  summarise(count=n())
+
+# Slide version 
+ggplot() +
+  geom_col(data = gva_plt, aes(x = month_year, y = value, fill = name)) +
+  stat_smooth(data = gv_plt, aes(x = month_year, y = count), se = FALSE, span = .2, color = "grey") +
+  labs(x = "",
+       y = "Number of Victims") +
+  scale_y_continuous(breaks = c(2, 4, 6, 8, 10, 12, 14, 16, 18)) +
+  scale_fill_manual(labels = c("Injured", "Killed"),
+                    values = c("#F8BE3D", "#007BAB"),
+                    guide = guide_legend(title = "Victim Status")) +
+  theme_minimal() +
+  theme(legend.position = "top",
+        legend.margin=margin(0,0,0,0),
+        legend.box.margin=margin(0,-10,-10,-10),
+        axis.text.x = element_text(angle = 25))
+
+# One-pager version
+ggplot() +
+  geom_col(data = gva_plt, aes(x = month_year, y = value, fill = name)) +
+  stat_smooth(data = gv_plt, aes(x = month_year, y = count), se = FALSE, span = .2, color = "darkgrey") +
+  labs(x = "",
+       y = "",
+       title = "Incidents of Gun Violence in Charlottesville & Albemarle and Number of Victims by Month",
+       caption = "Source: Gun Violence Archive (Victim Statuses) & Local Police Reports (Incident Counts)") +
+  scale_y_continuous(breaks = c(2, 4, 6, 8, 10, 12, 14, 16, 18)) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  scale_fill_manual(labels = c("Injured", "Killed"),
+                    values = c("#F8BE3D", "#007BAB"),
+                    guide = guide_legend(title = "Victim Status")) +
+  theme_bw() +
+  theme(legend.position = c(.88, .85),
+        legend.box.background = element_rect(color = "black"),
+        legend.title = element_text(size = 9),
+        axis.text.x = element_text(angle = 25)) +
+  annotate("label", x = mdy("1-1-2019"), y = 16, 
+           label = paste("Number of incidents (shots\nfired, assaults, and homicides"),
+           size = 3) +
+  geom_segment(aes(x = mdy("1-1-2019"), y = 15, xend = mdy("1-10-2020"), yend = 12),
+               arrow = arrow(length = unit(0.25, "cm")))
+
+
+
   
 
 
-gva %>%
+# N incidents 
+gv_narrow <- gv_plt %>%
+  filter(between(month_year, mdy("7-31-2022"), mdy("3-31-2023")))
+
+sum(gv_narrow$count)
+
+
+# Victims and Incidents over time by YEAR -- not as helpful
+
+gva_yr <- gva %>%
   filter(between(incident_date, mdy("12-31-2017"), mdy("12-31-2024"))) %>%
-  ggplot(aes(x = month_year)) +
-  geom_dotplot(method = "histodot")
+  group_by(yr = year(incident_date)) %>%
+  summarise(total_injured = sum(victims_injured + suspects_injured),
+            total_killed = sum(victims_killed + suspects_killed)) %>%
+  ungroup() %>%
+  pivot_longer(matches("total")) 
+
+gv_yr <- gv %>%
+  group_by(yr = year(reported_date)) %>% 
+  summarise(count=n())
 
 
-
-
-
-
-
-
+ggplot() +
+  geom_col(data = gva_yr, aes(x = yr, y = value, fill = name)) +
+  stat_smooth(data = gv_yr, aes(x = yr, y = count), se = FALSE, color = "darkgrey") +
+  labs(x = "Year",
+       y = "Number of Victims") +
+  scale_fill_manual(labels = c("Injured", "Killed"),
+                    values = c("#F8BE3D", "#007BAB"),
+                    guide = guide_legend(title = "Victim Status")) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 35)) +
+  scale_x_continuous(breaks = gva_yr$yr)
 
 
 # Childhood poverty ----
@@ -88,7 +170,8 @@ gv_sf_summary <- dat %>%
          locality = str_to_title(locality)) 
 
 scatter_text2 <- gv_sf_summary %>%
-  filter(cpov_est > 31)
+  filter(cpov_est > 31 |
+         percent_incidents > 5)
 
 gv_sf_summary %>%
   filter(tract_name != "JPA - Fontaine") %>%
@@ -107,10 +190,11 @@ gv_sf_summary %>%
        y = "Percentage of Total Gun Violence Incidents",
        title = "Childhood Poverty Rates Compared to Incidents of Gun Violence",
        subtitle = "Each circle represents a different census tract",
-       caption = "Years: 2018 - 2024",
+       caption = "Source: American Community Survey 2022 & Local Police Reports (2019-2024)",
        size = "Estimated Population") +
   guides(size = "none") +
-  theme_minimal()
+  theme_bw() +
+  theme(legend.position = "top")
 
 # Theft ----
 
@@ -121,7 +205,8 @@ ggplot(nibrs_theft, aes(year, n_stolen, colour = district)) +
   geom_line(linewidth = 1.5) +
   labs(x = "",
        y = "Number of Firearms Stolen",
-       title = "Theft of Firearms from Vehicles") +
+       title = "Theft of Firearms from Vehicles",
+       caption = "Source: VA State Police Unified Crime Reporting") +
   scale_color_manual(values = c("#007BAB", "#F8BE3D"), 
                      name = "Region") +
   geom_label(data = nibrs_theft, 
@@ -135,6 +220,4 @@ ggplot(nibrs_theft, aes(year, n_stolen, colour = district)) +
   theme(legend.position = "top")
 
 
-library(extrafont)
-loadfonts()
   

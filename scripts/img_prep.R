52f7ee7e6ef516c296a2d5c8aad862a764c58390
Samantha Toet <samanthactoet@gmail.com>
2025-04-02 14:17:18 -0400

    format images for slides

--- scripts/img_prep.R
+++ scripts/img_prep.R
@@ -40,26 +40,55 @@ gv_plt <- gv %>%
 
 # *Datawalk ----
 
-img <- readPNG("img/paper_light.png")
+#img <- readPNG("img/paper_light.png")
+# ggplot() +
+#   background_image(img) +
+#   geom_col(data = gva_plt, aes(x = month_year, y = value, fill = name), width = 25) +
+#   stat_smooth(data = gv_plt, aes(x = month_year, y = count), se = FALSE, span = .2, color = "#2f9aa0ff") +
+#   labs(x = NULL,
+#        y = NULL) +
+#   scale_y_continuous(breaks = c(2, 4, 6, 8, 10, 12, 14, 16, 18)) +
+#   scale_x_date(date_breaks = "1 year", date_labels = "%Y", date_minor_breaks="1 month") +
+#   scale_fill_manual(labels = c("Injured", "Killed"),
+#                     values = c("#440154FF", "#7AD151FF"),
+#                     guide = guide_legend(title = "Victim Status")) +
+#   theme_minimal() +
+#   theme(legend.position = "bottom") +
+#   annotate("label", x = mdy("1-1-2019"), y = 16, 
+#            label = paste("Number of incidents (shots\nfired, assaults, and homicides"),
+#            size = 3) +
+#   geom_segment(aes(x = mdy("1-1-2019"), y = 15, xend = mdy("1-10-2020"), yend = 12),
+#                arrow = arrow(length = unit(0.25, "cm"))) 
 
+# high-res for slides 
 ggplot() +
-  background_image(img) +
   geom_col(data = gva_plt, aes(x = month_year, y = value, fill = name), width = 25) +
   stat_smooth(data = gv_plt, aes(x = month_year, y = count), se = FALSE, span = .2, color = "#2f9aa0ff") +
   labs(x = NULL,
-       y = NULL) +
+       y = "Monthly Counts") +
   scale_y_continuous(breaks = c(2, 4, 6, 8, 10, 12, 14, 16, 18)) +
-  scale_x_date(date_breaks = "1 year", date_labels = "%Y", date_minor_breaks="1 month") +
+  scale_x_date(date_breaks = "6 months", date_labels = "%b '%y", date_minor_breaks="1 month") +
   scale_fill_manual(labels = c("Injured", "Killed"),
-                    values = c("#440154FF", "#7AD151FF"),
-                    guide = guide_legend(title = "Victim Status")) +
+                       values = c("#440154FF", "#7AD151FF"),
+                       guide = guide_legend(title = "Victim Status")) +
   theme_minimal() +
-  theme(legend.position = "bottom") +
-  annotate("label", x = mdy("1-1-2019"), y = 16, 
-           label = paste("Number of incidents (shots\nfired, assaults, and homicides"),
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(angle = 35),
+        panel.grid.major.x = element_line(size = 1.5)) +
+  annotate("label", x = mdy("11-1-2018"), y = 5.5, 
+           label = paste("Gun-related 911 calls\n(ex. shots fired or assaults)"),
            size = 3) +
-  geom_segment(aes(x = mdy("1-1-2019"), y = 15, xend = mdy("1-10-2020"), yend = 12),
-               arrow = arrow(length = unit(0.25, "cm"))) 
+  geom_segment(aes(x = mdy("7-1-2018"), y = 6.7, xend = mdy("12-20-2018"), yend = 7.8),
+               arrow = arrow(length = unit(0.20, "cm"))) +
+  geom_vline(xintercept = mdy("3-12-2020"), linetype = "dotted") +
+  annotate("label", x = mdy("11-15-2020"), y = 9, label = "COVID lockdown", size = 3) +
+  geom_segment(aes(x = mdy("7-1-2020"), y = 9.6, xend = mdy("4-1-2020"), yend = 10.5),
+               arrow = arrow(length = unit(0.2, "cm"))) +
+  annotate("label", x = mdy("2-15-2022"), y = 19, label = paste(
+    "Hearing or witnessing gun violence takes\na collective toll on our entire community"),
+    size = 3) +
+  geom_segment(aes(x = mdy("12-15-2020"), y = 19, xend = mdy("7-15-2020"), yend = 17.9),
+    arrow = arrow(length = unit(0.2, "cm"))) 
 
 # *One-pager ----
 ggplot() +
